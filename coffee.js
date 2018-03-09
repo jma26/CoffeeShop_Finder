593d50d463b963962a6175e0cf9593358a60a3e6
jma26 <j.masonoma17@gmail.com>
2018-03-08 23:03:26 -0800

    Solved bug where callback function is firing multiple times, avoid using bounds_change, use "idle"

--- coffee.js
+++ coffee.js
@@ -1,10 +1,9 @@
 function initMap() {
 
     if (navigator.geolocation) {
-        // getCurrentPosition accepts 2 parameters(successful coords, error)
-        navigator.geolocation.getCurrentPosition(currentPosition, defaultPosition);
+        navigator.geolocation.getCurrentPosition(currentPosition, defaultPosition)
     } else {
-        console.log("Geolocation is not supported by this browser");
+        alert('Geolocation is not supported on your browser');
     }
 
 
@@ -14,7 +13,7 @@ function initMap() {
 
         //  Map options
         let mapOptions = {
-            center: {lat: position.coords.latitude, lng: position.coords.longitude},
+            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
             zoom: 11
         }
 
@@ -24,11 +23,9 @@ function initMap() {
             'long': position.coords.longitude
         }
 
-        // Bounds for nearBySearch
-        let currentBounds = new google.maps.LatLng(location.lat, location.long);
         // nearbySearch request fields
         let request = {
-            location: currentBounds,
+            location: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
             type: ['cafe'],
             openNow: true,
             radius: 7000
@@ -39,14 +36,43 @@ function initMap() {
         // Map initialization
         let map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+        let service = new google.maps.places.PlacesService(map);
+
+        function callback(results, status) {
+            console.log('Function callback is hit');
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (let i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        }
+
+        let markers = [];
+        // Create a marker for each result from function callback(results, status)
+        function createMarker(place) {
+            let marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            markers.push(marker);
+            
+
+            // Add event map listener for marker 'click'
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(place.name + "<br> Rating:" + place.rating);
+                infowindow.open(map, this);
+            });
+        }
+
         // Bias results towards current maps' viewport
-        map.addListener('bounds_changed', function() {
+        google.maps.event.addListener(map, 'idle', function() {
             
             // Clear out old markers
             markers.forEach(function(marker) {
                 marker.setMap(null);
             });
             markers = [];
+            
             let lat = map.getBounds().getCenter().lat();
             let long = map.getBounds().getCenter().lng();
             let updatedBounds = new google.maps.LatLng(lat, long);
@@ -61,85 +87,39 @@ function initMap() {
             service.nearbySearch(request, callback);
 
         })
-        let service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
-
-        function callback(results, status) {
-            console.log('Function callback is hit');
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (let i = 0; i < results.length; i++) {
-                    createMarker(results[i]);
-                }
-            }
-        }
-
-        let markers = [];
-        // Create a marker for each result from function callback(results, status)
-        function createMarker(place) {
-            let marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location
-            });
-            markers.push(marker);
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(place.name + "<br> Rating:" + place.rating);
-                infowindow.open(map, this);
-            });
-        }
     }
 
     function defaultPosition() {
+        let count = 0;
         console.log('Default position executed');
+
+        // Initialize empty markers array
+        let markers = [];
+
+        // mapOptions
         let mapOptions = {
-            center: {lat: 37.4219999, lng: -122.08405749999997},
-            zoom: 11
+            center: {lat: 37.422000, lng: -122.084057},
+            zoom: 10
         }
 
-        // Map initialization 
+        // Map initialization
         let map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        // Infowindow initialization
-        let infowindow = new google.maps.InfoWindow();
-
-        // Bounds for nearBySearch
-        let currentBounds = new google.maps.LatLng(mapOptions.center.lat, mapOptions.center.lng);
 
         // nearbySearch request fields
         let request = {
-            location: currentBounds,
+            location: {lat: mapOptions.center.lat, lng: mapOptions.center.lng},
             type: ['cafe'],
             openNow: true,
             radius: 7000
         }
 
-        // Bias results towards current maps' viewport
-        map.addListener('bounds_changed', function() {
-
-            // Clear out old markers (Another way not using built-in method .forEach)
-            for (let i = 0; i < markers.length; i++) {
-                markers[i].setMap(null);
-            }
-            // markers.forEach(function(marker) {
-            //     marker.setMap(null);
-            // });
-            markers = [];
-
-            let lat = map.getBounds().getCenter().lat();
-            let long = map.getBounds().getCenter().lng();
-            let updatedBounds = new google.maps.LatLng(lat, long);
-
-            let request = {
-                location: updatedBounds,
-                type: ['cafe'],
-                openNow: true,
-                radius: 7000
-            }
-            service.nearbySearch(request, callback);
-        })
-
+        console.log(request);
+        
+        // infoWindow initialization
+        let infoWindow = new google.maps.InfoWindow;
+        
+        // nearbySearch initialization
         let service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
 
         function callback(results, status) {
             console.log('Function callback is hit');
@@ -150,7 +130,6 @@ function initMap() {
             }
         }
 
-        let markers = [];
         // Create a marker for each result from function callback(results, status)
         function createMarker(place) {
             let marker = new google.maps.Marker({
@@ -164,5 +143,29 @@ function initMap() {
                 infowindow.open(map, this);
             });
         }
+
+        // Bias results towards current maps' viewport
+        // NOTE: Known bug when using 'bounds_changed' where event gets triggered multiple times
+        google.maps.event.addListener(map, 'idle', function() {
+            // Clear out old markers
+            markers.forEach(function(marker) {
+                marker.setMap(null);
+            });
+            markers = [];
+            
+            let lat = map.getBounds().getCenter().lat();
+            let long = map.getBounds().getCenter().lng();
+            let updatedBounds = new google.maps.LatLng(lat, long);
+
+            let request = {
+                location: updatedBounds,
+                type: ['cafe'],
+                openNow: true,
+                radius: 7000
+            }
+
+            service.nearbySearch(request, callback);
+
+        })
     }
 }
